1bfb8285a013204e19a7fdb39eb6e84871460ca5
Iamzak <Iamzak@DESKTOP-UU563FV>
2023-01-20 18:26:58 +0300

    kullanici giris ekrani yapildi

--- WindowsFormsApp1/AllWindowsForms/GirisEkrani.cs
+++ WindowsFormsApp1/AllWindowsForms/GirisEkrani.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.Entity;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.Remoting.Contexts;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -25,12 +27,25 @@ namespace WindowsFormsApp1.AllWindowsForms
             kaydol.Show();
             this.Hide();
         }
-
+        UrunDbContext dbContext = new UrunDbContext();
         private void button1_Click(object sender, EventArgs e)
         {
-            Form1 form1 = new Form1();
-            form1.Show();
-            this.Hide();
+
+            string kullaniciAdi = textBox3.Text;
+            var tbl = dbContext.Kullanicis.FirstOrDefault(x => x.Ad == kullaniciAdi);
+            string sifre = textBox5.Text;
+            var tbl2 = dbContext.Kullanicis.FirstOrDefault(x => x.Sifre == sifre);
+            if (tbl==null||tbl2==null)
+            {
+                MessageBox.Show("Lütfen geçerli bir kullanıcı girin");
+                
+            }
+            else
+            {
+                Form1 form1 = new Form1();
+                form1.Show();
+                this.Hide();
+            }
         }
 
         private void button3_Click(object sender, EventArgs e)
